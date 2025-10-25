e4c8ff1fb9d33da230f647ddd2441a78315a6fef
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-10-25 10:05:56 -0400

    Remove unused routes for Payment and Confirmation pages
    Removed PaymentPage and ConfirmationPage routes from App component.

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,6 @@ import { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/LandingPage";
 import UserInfoPage from "./pages/UserInfoPage";
 import WalletPage from "./pages/WalletPage";
-import PaymentPage from "./pages/PaymentPage";
-import ConfirmationPage from "./pages/ConfirmationPage";
-
 export default function App() {
   return (
     <BrowserRouter>
@@ -16,8 +13,6 @@ export default function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/userinfo" element={<UserInfoPage />} />
         <Route path="/wallet" element={<WalletPage />} />
-        <Route path="/payment" element={<PaymentPage />} />
-        <Route path="/confirmation" element={<ConfirmationPage />} />
       </Routes>
     </BrowserRouter>
   );
