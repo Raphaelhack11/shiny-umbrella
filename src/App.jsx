7f24464eabb0e33834cb99c3f693c941332f0548
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-10-25 09:51:01 -0400

    Add Toaster and new routes for Payment and Confirmation

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,23 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
+
 import LandingPage from "./pages/LandingPage";
 import UserInfoPage from "./pages/UserInfoPage";
 import WalletPage from "./pages/WalletPage";
+import PaymentPage from "./pages/PaymentPage";
+import ConfirmationPage from "./pages/ConfirmationPage";
 
 export default function App() {
   return (
     <BrowserRouter>
+      <Toaster position="top-right" />
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/userinfo" element={<UserInfoPage />} />
         <Route path="/wallet" element={<WalletPage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/confirmation" element={<ConfirmationPage />} />
       </Routes>
     </BrowserRouter>
   );
