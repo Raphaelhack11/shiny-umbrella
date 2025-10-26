fc04d99dd7653cb3fb53ac0eb35e1bf782be6cc7
Raphaelhack11 <169177762+Raphaelhack11@users.noreply.github.com>
2025-10-26 02:41:55 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,30 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { Toaster } from "react-hot-toast";
-
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+import toast, { Toaster } from "react-hot-toast";
 import LandingPage from "./pages/LandingPage";
 import UserInfoPage from "./pages/UserInfoPage";
 import WalletPage from "./pages/WalletPage";
+import PaymentPage from "./pages/PaymentPage";
+import ConfirmationPage from "./pages/ConfirmationPage";
+
 export default function App() {
+  const [userData, setUserData] = useState({
+    name: "",
+    phone: "",
+    wallet: "",
+    balance: null,
+  });
+
   return (
-    <BrowserRouter>
-      <Toaster position="top-right" />
+    <Router>
+      <Toaster position="top-center" />
       <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/userinfo" element={<UserInfoPage />} />
-        <Route path="/wallet" element={<WalletPage />} />
+        <Route path="/" element={<LandingPage setUserData={setUserData} />} />
+        <Route path="/userinfo" element={<UserInfoPage setUserData={setUserData} />} />
+        <Route path="/wallet" element={<WalletPage userData={userData} />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/confirmation" element={<ConfirmationPage />} />
       </Routes>
-    </BrowserRouter>
+    </Router>
   );
 }
